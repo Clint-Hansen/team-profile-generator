b2fd92920dd0ef97fa7d3e83d216521ea73a0cbf
Clint Hansen <clint.hansen2009@gmail.com>
2021-03-30 18:39:42 -0600

    employee jest tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,35 @@
 const Employee = require('../lib/Employee.js')
+
+test('creates a new employee', () => {
+    const employee = new Employee('Bill', '123', 'bill@mail.com', 'Manager');
+
+    expect(employee.name).toBe('Bill')
+    expect(employee.id).toBe('123');
+    expect(employee.email).toBe('bill@mail.com');
+    expect(employee.role).toBe('Manager')
+});
+
+test('returns the employee name', () => {
+    const employee = new Employee('Bill');
+
+    expect(employee.name).toBe('Bill')
+
+});
+
+test('returns the employee id', () => {
+    const employee = new Employee('Bill', '123', 'bill@mail.com', 'Manager');
+
+    expect(employee.id).toBe('123');
+});
+
+test('returns employee email address', () => {
+    const employee = new Employee('Bill', '123', 'bill@mail.com', 'Manager');
+
+    expect(employee.email).toBe('bill@mail.com')
+});
+
+test('returns employee role', () => {
+    const employee = new Employee('Bill', '123', 'bill@mail.com', 'Manager');
+
+    expect(employee.role).toBe('Manager');
+});
